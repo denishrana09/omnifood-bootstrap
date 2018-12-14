602331347f9c6fcb0fd52dd5f296250cb05f60ce
denish rana <denishrana09@gmail.com>
2018-12-14 16:11:10 +0530

    feat: jquery added for anchor links

--- js/script.js
+++ js/script.js
@@ -47,6 +47,28 @@ $(document).ready(function() {
       offset: '30%;'
     });
 
+    $('.scroll-to-plans').click(function () {
+        $('html, body').animate({scrollTop: $('.plans-section').offset().top}, 1000);
+     });
+
+     $('.scroll-to-start').click(function () {
+        $('html, body').animate({scrollTop: $('.features-section').offset().top}, 1000);
+     });
+
+    $(function() {
+        $('a[href*=\\#]:not([href=\\#])').click(function() {
+          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+              $('html,body').animate({
+                scrollTop: target.offset().top - 90
+              }, 1000);
+              return false;
+            }
+          }
+        });
+    });
 });
 
 // document.addEventListener("scroll", () => {
